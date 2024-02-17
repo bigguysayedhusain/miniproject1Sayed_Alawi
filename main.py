12fc060faa3f62784b723aee2c917a63dbeb8b3a
Sayed Husain Alawi <shalawi@mail.fhsu.edu>
2024-02-17 13:00:40 +0300

    Finished Basic matplotlib coding

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import yfinance as yf
 import copy
 from pathlib import Path
 
-tickers = ['MSFT'] # TODO add 'TSLA', 'SONY', 'META', 'AAPL' back to tickers
+tickers = ['MSFT']  # TODO add 'TSLA', 'SONY', 'META', 'AAPL' back to tickers
 
 try:
     Path('charts').mkdir()
@@ -17,8 +17,24 @@ except FileExistsError:
 
 for i in tickers:
     data = (yf.Ticker(i)).history(period='10d')
-    closing_prices = []
 
+    closing_prices_list = []
+
+    for i in data['Close']:
+        closing_prices_list.append(i)
+
+    closing_prices_array = np.array(closing_prices_list)
+
+    plt.plot(closing_prices_array)
+    plt.xlabel('Days')
+    plt.ylabel('Closing Price')
+    plt.title(f"{tickers} Closing Prices")
+    plt.axis((1, 10, (min(closing_prices_list) - 2), max(closing_prices_list) + 2))
+
+    plt.show()
+    # print(closing_prices)
+    # min(closing_prices)
+    # max(closing_prices)
 
 # TODO delete Temp.py
 # TODO Don't forget to fulfill the requirements.txt
