1089d485906398179caa9a0f1261bfcdc34d8d5e
Sayed Husain Alawi <shalawi@mail.fhsu.edu>
2024-02-17 12:29:32 +0300

    Extracting the data from yfinance

--- main.py
+++ main.py
@@ -8,10 +8,18 @@ import yfinance as yf
 import copy
 from pathlib import Path
 
-tickers = ['MSFT', 'TSLA', 'SONY', 'META', 'AAPL']
+tickers = ['MSFT'] # TODO add 'TSLA', 'SONY', 'META', 'AAPL' back to tickers
 
 try:
     Path('charts').mkdir()
 except FileExistsError:
     pass
 
+for i in tickers:
+    data = (yf.Ticker(i)).history(period='10d')
+    closing_prices = []
+
+
+# TODO delete Temp.py
+# TODO Don't forget to fulfill the requirements.txt
+# TODO Don't forget to finish the README.md
